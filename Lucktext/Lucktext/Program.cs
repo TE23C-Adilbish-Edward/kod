f821e2d6cae732c2e2c2123c211ce436a76be74b
Edward Adilbish <edward.adilbishvilen@elev.ga.ntig.se>
2024-08-30 13:56:57 +0200

    Första

---
+++ Lucktext/Lucktext/Program.cs
@@ -0,0 +1,18 @@
+﻿string name;
+
+Console.WriteLine("Skriv in ditt namn");
+name = Console.ReadLine();
+
+string mat;
+
+Console.WriteLine("Skriv in din favoriträtt");
+mat = Console.ReadLine();
+
+string verb;
+
+Console.WriteLine("Skriv in ett nummer");
+verb = Console.ReadLine();
+
+Console.Clear();
+Console.WriteLine(name + " åt " + mat + " till middag och senare gick " + name + " och vann " + verb + "kr på casinot");
+Console.ReadLine();
